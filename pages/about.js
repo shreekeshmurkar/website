ea8d20058078052c843956d1972d836ddaf43472
Anjali Sharma <anjali.rsharma29@gmail.com>
2022-11-15 20:36:20 +0530

    read data from files

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,28 @@
 import Link from "next/link";
 import Head from "next/head";
+import { attributes } from '../content/about.md';
 
-export default function About() {
-    return (
-      <>
-        <Head>
-          <title>About</title>
-          <meta name="description" content="We build amazing things" />
-        </Head>
-        <h1>About Page</h1>
-        <Link href="/">Back to Hpme</Link>
-      </>
-    ); 
+export default function About({ attr }) {
+    return ( <
+        >
+        <
+        Head >
+        <
+        title > About Us < /title> <
+        meta name = "description"
+        content = "We build amazing things" / >
+        <
+        /Head> <
+        h1 > { attr.metaTitle } < /h1> <
+        h2 > { attr.metaDescription } < /h2> <
+        Link href = "/" > Back to Hpme < /Link> <
+        />
+    );
+}
+export async function getStaticProps() {
+    return {
+        props: {
+            attr: attributes,
+        },
+    };
 }
