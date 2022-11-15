6b3e9896fad7663b1ff14922570b973f738a4b04
Anjali Sharma <anjali.rsharma29@gmail.com>
2022-11-15 20:37:47 +0530

    save file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withMDX = require('@next/mdx')();
+const Mode = require('frontmatter-markdown-loader/mode');
 
-module.exports = nextConfig
+module.exports = withMDX({
+    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+    webpack(configuration) {
+        configuration.module.rules.push({
+            test: /\.md$/,
+            loader: 'frontmatter-markdown-loader',
+            options: {
+                mode: [Mode.REACT],
+            },
+        });
+
+        return configuration;
+    }
+});
