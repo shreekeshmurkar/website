643c992f8587e4a06fc84446951e1e1a3b7bb612
Anjali Sharma <anjali.rsharma29@gmail.com>
2022-11-16 15:22:37 +0530

    Careers page

--- pages/careers.js
+++ pages/careers.js
@@ -1,17 +1,47 @@
 import Head from "next/head";
 import Link from "next/link";
+import styles from "../styles/Careers.module.css";
 export default function Careers() {
   return (
     <>
       <Head>
         <title>Careers</title>
-        <meta
-          name="description"
-          content="Careers at KA"
-        />
+        <meta name="description" content="Careers at KA" />
       </Head>
-      <h1>Careers Page</h1>
-      <Link href="/">Back to Home</Link>
+      <h2>Join our team</h2>
+
+      <>
+        <div className={styles.card}>
+          <h2> Software Engineer DotNet</h2>
+          <p> Looking for DotNet , Mvc ,.NetCore, SQL expertise individual </p>
+          <Link
+            className={styles.applyLink}
+            href="https://airtable.com/shrd8wffs2FpB1J8h"
+          >
+            Apply
+          </Link>
+        </div>
+        <div className={styles.card}>
+          <h2> Software Engineer JAVA</h2>
+          <p> Technologies Needed: Java, Angular , MVC & SQL </p>
+          <Link
+            className={styles.applyLink}
+            href="https://airtable.com/shrd8wffs2FpB1J8h"
+          >
+            Apply
+          </Link>
+        </div>
+        <div className={styles.card}>
+          <h2> Software Engineer REACT</h2>
+          <p> Technologies Needed: JS, React , NextJs & Vue Js </p>
+          <Link
+            className={styles.applyLink}
+            href="https://airtable.com/shrd8wffs2FpB1J8h"
+          >
+            Apply
+          </Link>
+        </div>
+      </>
     </>
   );
 }
