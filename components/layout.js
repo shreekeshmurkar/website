8ad4991495fbdb7fde0fc0e0850a8f462cbd76f2
shreekeshmurkar <shreekeshmurkar@gmail.com>
2022-11-16 18:51:07 +0530

    layout changes

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,14 @@
 import Header from "./Header/header";
-import Footer from "./Footer/footer";
+import Footer from "./Footer/Footer";
 import styles from "./Layout.module.css";
 
 const Layout = (props) => {
+  const brandname="Kiprosh America";
   return (
     <div>
-      <Header brandname="Kiprosh America"/>
+      <Header brandname={brandname}/>
       <div className={styles["page-container"]}>{props.children}</div>
-      <Footer />
+      <Footer brandname={brandname}/>
     </div>
   );
 };
