6081c40065d138ce9228923c1e614b44b34806f8
Anjali Sharma <anjali.rsharma29@gmail.com>
2022-11-16 20:26:48 +0530

    Show active page

--- components/Header/NavList.js
+++ components/Header/NavList.js
@@ -1,20 +1,24 @@
 import { Fragment } from "react";
 import Link from "next/link";
+import withRouter from "next/dist/client/with-router";
+import styles from "./Header.module.css";
 
 const NavItem =  (props) => {
+  const activeClass= props.isActive? styles.isActiveRoute
+    :"";  
     return (
-      <Link href={props.route}>
+      <Link href={props.route} className = {activeClass}>
         {props.value}
       </Link>
     );
 }
-const NavList = (props) => {
+const NavList = ({router, items}) => {
     return <Fragment>
-        {props.items.map((item) => {
-            return <NavItem key={item.id} route={item.route} value={item.value} />
+        {items.map((item) => {
+            return <NavItem isActive={router.pathname === item.route} key={item.id} route={item.route} value={item.value} />
         })}
     </Fragment>
 }
 
 
-export default NavList;
+export default withRouter(NavList);
