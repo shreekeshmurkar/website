ef56c7a29db2526b21783870e4fcfac9b9afedcc
shreekeshmurkar <shreekeshmurkar@gmail.com>
2022-11-17 19:08:07 +0530

    added changes for product page

---
+++ content/product.md
@@ -0,0 +1,20 @@
+---
+featureSectionTitile: Why to choose Kiprosh America?
+features:
+  - title: Elegant user interface
+    image: /Logo.svg
+    description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  - title: Latest technology stack
+    image: /Logo.svg
+    description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  - title: Performent
+    image: /Logo.svg
+    description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+productTitle: Time Tracking App
+productDescription: App to track working time.
+productImages:
+  - src: /images/Product1.jpg
+  - src: /images/Product2.jpg
+  - src: /images/Product3.jpg
+productLink: https://timetrack.azurewebsites.net
+---
--- pages/product.js
+++ pages/product.js
@@ -1,3 +1,45 @@
-export default function Product() {
-    return (<h1>Product</h1>)
+import Image from "next/image";
+import styles from "../styles/Product.module.css";
+import { attributes } from "../content/product.md";
+import Link from "next/link";
+export default function Product(props) {
+    return (
+    <>
+        <Features {...props} />
+        <h1 className={styles.title}>{props.attr.productTitle}</h1>
+        <div className={styles["product-container"]}>
+        {
+            props.attr.productImages.map(image => {
+                return <div className={styles["product-image"]}><Image height="300" width="400" src={image.src} alt="logo"></Image></div>
+            })
+        }
+        </div>
+        <div className={styles["product-link"]}><Link href={props.attr.productLink}>Explore</Link></div>
+    </>
+    )
 }
+
+const Features = (props) => {
+    return <>
+        <h1 className={styles.title}>{props.attr.featureSectionTitile}</h1>
+        <div className={styles["feature-container"]}>
+        {
+            props.attr.features.map(feature => {
+                return <div className={styles.feature}>
+                <div className={styles["feature-image"]}><Image height="50" width="50" src={feature.image} alt="logo"></Image></div>
+                <div className={styles["feature-title"]}><span>{feature.title}</span></div>
+                <div className={styles["feature-description"]}><span>{feature.description}</span></div>
+            </div>
+            })
+        }
+        </div> 
+    </>
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+           attr: attributes,
+        },
+      };
+} 
