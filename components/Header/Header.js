909bc7f42fa882e15ee300c11e46ffd5362787e3
Anjali Sharma <anjali.rsharma29@gmail.com>
2022-11-17 11:44:33 +0530

    Removed back btn

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import NavList from "./NavList";
 import styles from "./Header.module.css";
+import Link from "next/link";
 const Header = (props) => {
 
     const items = [{
@@ -16,7 +17,7 @@ const Header = (props) => {
 
     return (
       <div className={styles["header-container"]}>
-        <div className={styles["brand-container"]}>
+        <Link href="/">  <div className={styles["brand-container"]}>
           <Image
             width="50"
             height="50"
@@ -26,6 +27,7 @@ const Header = (props) => {
           />
           <div className={styles.brandname}>{props.brandname}</div>
         </div>
+       </Link>
         <div className={styles.menu}>
           <NavList items={items} />
         </div>
